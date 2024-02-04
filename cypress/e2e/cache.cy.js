2cefb0c330f5b0e8ffe5f4ca664a98cdc5ce924d
Tomek <tomasz.gajda1307@gmail.com>
2024-02-04 11:40:56 +0100

    Remove files

--- cypress/e2e/cache.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Cache Tests', () => {
-    it('Check resource caching', () => {
-      cy.reload(true)
-    // Force page reload with cache clearing
-    // Test if resources are correctly cached
-  })
-})
-  
--- cypress/e2e/other.cy.js
+++
@@ -1,9 +0,0 @@
-describe('CallBack', () => {
-    it('Correct', () => {
-      cy.visit('/#dashboard', {
-        onBeforeLoad: (contentWindow) => {
-    // contentWindow is the remote page's window object
-     },
-   })
-  })
-})
